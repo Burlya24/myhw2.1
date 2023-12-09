33e7c6b22ae132ae3084ff8d854da62aafb1022d
Olga Burlakova <jkzz9797@mail.ru>
2023-12-09 15:11:14 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 10_000;
+        int about = 20;
+        int bonus = price / about;
+        System.out.println("Итоговый бонус:" + bonus);
+    }
+
+}
+
+
